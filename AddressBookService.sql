b4fb401f2e481aa8b360cc363600752291879c63
Remant <remantmahato9798@gmail.com>
2022-04-23 06:44:48 +0530

    Added : UC2-CreateTable

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,14 @@
 --UC1-Create Database
 create database AddressBookServiceDB;
+-- UC 2: Create Table 
+create table AddressBookTable
+(FirstName varchar(100),
+SecondName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200)
+)
+select * from AddressBookTable
